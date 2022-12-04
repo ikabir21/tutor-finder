3d20a3061a6f87bbb236d4d945c880b4a7955869
nandan-shah <nandan2583@gmail.com>
2022-12-04 14:21:16 +0530

    added tutor profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
-import React from "react";
+import React from 'react';
 
 // MUI Imports
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Project Imports
-import theme from "./assets/theme";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
+import theme from './assets/theme';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import TutorProfile from './pages/TutorProfile';
 
 const App = () => {
-	return (
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<Router>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Home />}></Route>
-				</Routes>
-			</Router>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/tutor' element={<TutorProfile />}></Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ client/src/components/StudentReview.js
@@ -0,0 +1,19 @@
+import { Card, CardHeader, Avatar } from '@mui/material';
+import React from 'react';
+
+const StudentReview = () => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: '#545' }} aria-label='recipe'>
+            R
+          </Avatar>
+        }
+        title='Shrimp and Chorizo Paella'
+        subheader='Some Travel Plans! We hope If you wish to unsubscribe, please click her'
+      />
+    </Card>
+  );
+};
+export default StudentReview;
